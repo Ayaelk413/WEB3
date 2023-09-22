068ae62e91b483499a77a4ee9ae57af95ead1da2
Aya elk <aya.elhakkouni@student.vinci.be>
2023-09-22 12:44:09 +0200

    module2 : init

--- exercices/module2/src/Components/App/App.jsx
+++ exercices/module2/src/Components/App/App.jsx
@@ -1,20 +1,38 @@
+//import React from 'react';
 import { useState } from 'react'
+import Display from '../Display/Display'
+import Button from '../Button/Button'
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
 
-  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
+  const increaseByOne = () => {
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
 
-  console.log('rendering....',counter)
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
+    </div>
   )
-}
-
+} 
 export default App
