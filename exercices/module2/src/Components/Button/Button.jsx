2ac7c8d14cd5f5afe068a7c5b00667a07d454d34
Ayaelk <aya.elhakkouni@student.vinci.be>
2023-10-06 12:08:19 +0200

    module2 : localStorage

--- exercices/module2/src/Components/Button/Button.jsx
+++ exercices/module2/src/Components/Button/Button.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const Button = ({ changeCount, text, delta }) => {
-  const handleClick = (e) => {
-    const parsedDelta = parseInt(e.target.dataset.delta, 10);
-    changeCount(parsedDelta);
+  const handleClick = () => {
+    changeCount(delta);
   };
 
   return (
-    <button 
-    onClick={handleClick} data-delta={delta}>{text}
+    <button onClick={handleClick}>
+      {text}
     </button>
   );
 };
