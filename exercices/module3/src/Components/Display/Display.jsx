2c33763eed57c568c8fae971ecb125c4a7eec5d4
Ayaelk <aya.elhakkouni@student.vinci.be>
2023-10-13 12:46:36 +0200

    module3 : Tutoriel

--- exercices/module3/src/Components/Display/Display.jsx
+++
@@ -1,5 +0,0 @@
-const Display = ({ counter }) => 
-<div>
-  {counter}
-</div>
-  export default Display
--- exercices/module3/tutoriel/src/Components/App/App.jsx
+++ exercices/module3/tutoriel/src/Components/App/App.jsx
@@ -1,8 +1,11 @@
 
 import { useState } from 'react'
+//import useLocalStorage from '../../hooks/useLocalStorage'
+import Button from '../Button/Button'
+import Display from '../Display/Display'
 
 
-const History = (props) => {
+/*const History = (props) => {
   if (props.allClicks.length === 0) {
     return (
       <div>
@@ -36,8 +39,10 @@ const App = () => {
     console.log('left before', left)
     setRight(right + 1)
     console.log('left after', left)
-    setTotal(left + right)
+    setTotal(left + right) //or  setTotal(left + right) 
   }
+
+
 
   return (
     <div>
@@ -47,6 +52,26 @@ const App = () => {
     {right}
     <History allClicks={allClicks} />
   </div>
+  )
+}*/
+
+//Régle des crochets sur https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps
+//Programmeurs de programmes Web serment à voir c'est interessant !
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />      
+      <Button handleClick={() => setToValue(0)} text="reset" />      
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
   )
 }
 export default App;
