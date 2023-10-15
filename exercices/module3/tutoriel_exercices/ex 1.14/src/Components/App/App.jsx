7ade882364472a9c8741c0b553ae182e25adb10c
Aya <aya.elhakkouni@student.vinci.be>
2023-10-15 14:42:17 +0200

    module3: tutoriel_ex1.14

---
+++ exercices/module3/tutoriel_exercices/ex 1.14/src/Components/App/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ];
+
+  const initialVotes = new Array(anecdotes.length).fill(0);
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(initialVotes);
+
+  const selectRandomAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  const voteForAnecdote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
+  // Trouver l'indice de l'anecdote avec le plus grand nombre de votes
+  const maxVotes = Math.max(...votes);
+  const mostVotedIndex = votes.indexOf(maxVotes);
+
+  return (
+    <div>
+      <div>
+        <h1>Anecdote of the Day</h1>
+        {anecdotes[selected]}
+      </div>
+      <div>
+        <p>Has {votes[selected]} votes</p>
+        <button onClick={voteForAnecdote}>Vote</button>
+        <button onClick={selectRandomAnecdote}>Next Anecdote</button>
+      </div>
+      <div>
+        <h1>Most Voted Anecdote</h1>
+        {maxVotes > 0 ? anecdotes[mostVotedIndex] : 'No votes yet.'}
+      </div>
+    </div>
+  );
+};
+
+export default App;
